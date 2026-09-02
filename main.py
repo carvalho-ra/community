c17b892dcadfced7f9190d0b90ce1f5cadbe8f7f
Rodrigo <rodrigoadcarvalho@gmail.com>
2026-09-02 10:47:16 -0300

    feat: python lists and for in html

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+lista_users = ['Rodrigo', 'Rafael', 'Fernanda', 'Alon', 'Flávia']
+
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -10,6 +12,10 @@ def home():
 @app.route("/contato")
 def contato():
     return render_template("contato.html")
+
+@app.route("/users")
+def users():
+    return render_template("users.html", lista_users=lista_users)
 
 
 if __name__ == '__main__':
