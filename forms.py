97a04749d3076200e712c6f6dcc56d94257d348a
Rodrigo <rodrigoadcarvalho@gmail.com>
2026-09-02 11:41:36 -0300

    feat: created python form classes

---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class FormCreateAccount(FlaskForm):
+    username = StringField("User", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(6, 20)])
+    confirm_pwd = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    btn_submit_create_account = SubmitField("Create account")
+
+
+class FormLogin(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(6, 20)])
+    btn_submit_login = SubmitField("Create account")
