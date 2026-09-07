3893d5bde9083fe14ecf294ee2143da7aec5d83d
Rodrigo <rodrigoadcarvalho@gmail.com>
2026-09-07 13:36:56 -0300

    feat: email duplicity with flask ValidationError

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
 
 class FormCreateAccount(FlaskForm):
@@ -10,6 +11,10 @@ class FormCreateAccount(FlaskForm):
     confirm_pwd = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
     btn_submit_create_account = SubmitField("Create account")
 
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email já cadastrado.')
 
 class FormLogin(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
